6e9cc5fb72ff7202b6027c0f51d607bb9c4829d8
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2022-07-18 20:01:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 
-    //Connecting to Redis server on localhost 
-	$redis = new Redis(); 
-	$redis->connect('redis', 6379); 
-	echo "Connection to server sucessfully"; 
-	//check whether server is running or not 
-	echo "Server is running: ".$redis->ping(); 
- 
-
-
-   //database connection config
+      //database connection config
 	$hostname="db";
 	$db_user="root";
 	$db_pass="root";
