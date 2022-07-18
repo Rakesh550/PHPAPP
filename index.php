81a867c2a0bcbf8907cd2b0954a8ea2df204bab2
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2022-07-18 19:32:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
    $redis->connect('127.0.0.1', 6379); 
    echo "Connection to server sucessfully"; 
    // Get the stored keys and print it 
-   $arList = $redis->keys("*"); 
-   echo "Stored keys in redis:: " 
-   print_r($arList); 
+   #$arList = $redis->keys("*"); 
+   #echo "Stored keys in redis:: " 
+   #print_r($arList); 
 
-   
+
    //database connection config
 	$hostname="db";
 	$db_user="root";
